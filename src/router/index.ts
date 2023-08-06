1a9bde7892bf29ec14caa39fc5fc3744e1974be0
Bakhodirov-Jakhongir <bahodirovjahongirxoja@gmail.com>
2023-08-06 08:11:33 +0500

    create todo layout and types

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import EventList from "../views/EventList.vue";
 import EventDetails from "../views/EventDetails.vue";
 import About from "../views/About.vue";
+import TodoList from "@/views/TodoList.vue";
 
 const routes = [
   {
@@ -14,6 +15,11 @@ const routes = [
     name: "EventDetails",
     props: true,
     component: EventDetails,
+  },
+  {
+    path: "/todo-list",
+    name: "TodoList",
+    component: TodoList,
   },
   {
     path: "/about",
